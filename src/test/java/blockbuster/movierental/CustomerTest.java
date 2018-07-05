4737bac895398bf38f2ed91754f41b8d5c80151e
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2018-07-05 19:41:27 -0400

    Add test for customer with one day regular rental

--- src/test/java/blockbuster/movierental/CustomerTest.java
+++ src/test/java/blockbuster/movierental/CustomerTest.java
@@ -8,6 +8,8 @@ import static com.google.common.truth.Truth.assertThat;
 public class CustomerTest {
 
     private static final String NAME = "NAME";
+    private static final Movie REGULAR_MOVIE = new Movie("MOVIE", Movie.REGULAR);
+    private static final Rental REGULAR_ONE_DAY = new Rental(REGULAR_MOVIE, 1);
     private Customer customer;
 
     @Before
@@ -18,6 +20,13 @@ public class CustomerTest {
     @Test
     public void givenCustomerWithNoRentals_shouldProduceMinimalStatement() throws Exception {
         String expected = "Rental Record for NAME\nYou owed 0.0\nYou earned 0 frequent renter points\n";
+        assertThat(customer.statement()).isEqualTo(expected);
+    }
+
+    @Test
+    public void customerWithRegularOneDayRental() {
+        customer.addRental(REGULAR_ONE_DAY);
+        String expected = "Rental Record for NAME\n\tMOVIE\t2.0\nYou owed 2.0\nYou earned 1 frequent renter points\n";
         assertThat(customer.statement()).isEqualTo(expected);
     }
 }
