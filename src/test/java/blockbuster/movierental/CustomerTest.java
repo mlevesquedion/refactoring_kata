d8fcdd0c83065b2a45361b9efcc7a0681b237996
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2018-07-05 19:32:43 -0400

    Add test for minimal statement

--- src/test/java/blockbuster/movierental/CustomerTest.java
+++ src/test/java/blockbuster/movierental/CustomerTest.java
@@ -1,10 +1,23 @@
 package blockbuster.movierental;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static com.google.common.truth.Truth.assertThat;
+
 public class CustomerTest {
+
+    private static final String NAME = "NAME";
+    private Customer customer;
+
+    @Before
+    public void setUp() throws Exception {
+        customer = new Customer(NAME);
+    }
+
     @Test
-    public void should_do_something() throws Exception {
-        // TODO: implement test case
+    public void givenCustomerWithNoRentals_shouldProduceMinimalStatement() throws Exception {
+        String expected = "Rental Record for NAME\nYou owed 0.0\nYou earned 0 frequent renter points\n";
+        assertThat(customer.statement()).isEqualTo(expected);
     }
 }
