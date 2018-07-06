dbf281a22202436161e090609e29d3692772cdfb
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2018-07-05 20:09:52 -0400

    Add test for new release two days rental

--- src/test/java/blockbuster/movierental/CustomerTest.java
+++ src/test/java/blockbuster/movierental/CustomerTest.java
@@ -13,6 +13,7 @@ public class CustomerTest {
     private static final Rental REGULAR_THREE_DAYS = new Rental(REGULAR_MOVIE, 3);
     private static final Movie NEW_RELEASE_MOVIE = new Movie("NEW", Movie.NEW_RELEASE);
     private static final Rental NEW_RELEASE_ONE_DAY = new Rental(NEW_RELEASE_MOVIE, 1);
+    private static final Rental NEW_RELEASE_TWO_DAYS = new Rental(NEW_RELEASE_MOVIE, 2);
     private Customer customer;
 
     @Before
@@ -44,6 +45,13 @@ public class CustomerTest {
     public void customerWithNewReleaseOneDayRental() {
         customer.addRental(NEW_RELEASE_ONE_DAY);
         String expected = getStatement("\tNEW\t3.0\n", "3.0", "1");
+        assertThat(customer.statement()).isEqualTo(expected);
+    }
+
+    @Test
+    public void customerWithNewReleaseTwoDaysRental() {
+        customer.addRental(NEW_RELEASE_TWO_DAYS);
+        String expected = getStatement("\tNEW\t6.0\n", "6.0", "2");
         assertThat(customer.statement()).isEqualTo(expected);
     }
 
