da38ba4e12115ce8080962448bfe2273977c39ef
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2018-07-05 22:08:08 -0400

    Introduce RegularMovie subclass

--- src/test/java/blockbuster/movierental/MovieTest.java
+++ src/test/java/blockbuster/movierental/MovieTest.java
@@ -6,7 +6,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 public class MovieTest {
 
-    private static final Movie REGULAR = new Movie("REGULAR", Movie.REGULAR);
+    private static final Movie REGULAR = new RegularMovie("REGULAR");
     private static final Movie CHILDRENS = new Movie("CHILDRENS", Movie.CHILDRENS);
     private static final Movie NEW = new Movie("NEW", Movie.NEW_RELEASE);
 
