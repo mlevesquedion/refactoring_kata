0e8f67728eb17f4a70a755b2bbe3f95650eecdd5
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2018-07-05 21:40:39 -0400

    Fix typo

--- src/test/java/blockbuster/movierental/MovieTest.java
+++ src/test/java/blockbuster/movierental/MovieTest.java
@@ -42,11 +42,11 @@ public class MovieTest {
 
     @Test
     public void regularMovie_priceForTwoDays() {
-        assertThat(NEW.priceFor(2)).isWithin(1e-12).of(0.0);
+        assertThat(REGULAR.priceFor(2)).isWithin(1e-12).of(0.0);
     }
 
     @Test
     public void regularMovie_priceForFiveDays() {
-        assertThat(NEW.priceFor(5)).isWithin(1e-12).of(4.5);
+        assertThat(REGULAR.priceFor(5)).isWithin(1e-12).of(4.5);
     }
 }
