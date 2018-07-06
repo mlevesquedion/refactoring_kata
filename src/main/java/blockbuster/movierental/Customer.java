4f054991a137a86174d28a486a8a168a7fe6ddba
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2018-07-05 20:35:35 -0400

    Consolidate conditional

--- src/main/java/blockbuster/movierental/Customer.java
+++ src/main/java/blockbuster/movierental/Customer.java
@@ -38,10 +38,14 @@ public class Customer {
 
     private int addRenterPoints(int frequentRenterPoints, Rental rental) {
         frequentRenterPoints++;
-        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
+        if (isEligibleForExtraPoints(rental)) {
             frequentRenterPoints++;
         }
         return frequentRenterPoints;
+    }
+
+    private boolean isEligibleForExtraPoints(Rental rental) {
+        return rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1;
     }
 
     private double calculateCost(Rental rental) {
